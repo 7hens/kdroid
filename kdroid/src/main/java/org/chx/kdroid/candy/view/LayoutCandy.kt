0b3e8562d13b778910f8b18e3da96bd04257df5c
7hens <thenstango@live.com>
2017-03-16 11:30:19 +0800

    :) 添加 View 的 Candy

--- kdroid/src/main/java/org/chx/kdroid/candy/view/LayoutCandy.kt
+++ kdroid/src/main/java/org/chx/kdroid/candy/view/LayoutCandy.kt
@@ -1,11 +1,10 @@
-package org.chx.kdroid.candy.context
+package org.chx.kdroid.candy.view
 
 import android.content.Context
 import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 
 fun Context.inflate(@LayoutRes resId: Int, root: ViewGroup? = null, attachToRoot: Boolean = root != null): View
         = LayoutInflater.from(this).inflate(resId, root, attachToRoot)
