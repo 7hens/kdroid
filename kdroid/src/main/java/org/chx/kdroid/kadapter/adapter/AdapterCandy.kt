7194746c34456ded9490fe4db53d5b0b3cceae14
7hens <thenstango@live.com>
2017-03-26 16:11:32 +0800

    :) 更新 KAdapter

--- kdroid/src/main/java/org/chx/kdroid/kadapter/adapter/AdapterCandy.kt
+++ kdroid/src/main/java/org/chx/kdroid/kadapter/adapter/AdapterCandy.kt
@@ -1,16 +1,30 @@
 package org.chx.kdroid.kadapter.adapter
 
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import org.chx.kdroid.kadapter.KAdapter
+import android.support.v7.widget.RecyclerView
+import android.widget.AdapterView
 import org.chx.kdroid.kadapter.HolderView
+import org.chx.kdroid.kadapter.KAdapter
 import kotlin.reflect.KClass
 
-fun <D> KAdapter<D>.listAdapter() = YaListAdapter(this)
+fun <D> HolderView.Factory<D>.adapt(view: RecyclerView) =
+        YaRecyclerAdapter(this).apply { view.adapter = this }
 
-fun <D> KAdapter<D>.pagerAdapter(boundless: Boolean = false) = YaPagerAdapter(this, boundless)
+fun <D> KAdapter<D>.adapt(view: AdapterView<*>) =
+        YaListAdapter(this).apply { view.adapter = this }
 
-fun <D> HolderView.Factory<D>.recyclerAdapter() = YaRecyclerAdapter(this)
+fun <D> KAdapter<D>.adapt(view: ViewPager, boundless: Boolean = false) =
+        YaPagerAdapter(this, boundless).apply {
+            view.adapter = this
+            view.currentItem = this.firstPosition
+        }
 
-fun List<KClass<out Fragment>>.pagerAdapter(activity: AppCompatActivity, boundless: Boolean = false)
-        = YaFragmentPagerAdapter(activity.supportFragmentManager, this.map { it.java }, boundless)
+fun List<KClass<out Fragment>>.adapt(view: ViewPager, boundless: Boolean = false) =
+        (view.context as AppCompatActivity).supportFragmentManager.let {
+            YaFragmentPagerAdapter(it, this.map { it.java }, boundless).apply {
+                view.adapter = this
+                view.currentItem = this.firstPosition
+            }
+        }
--- sample/src/main/java/org/chx/kdroid/sample/MainActivity.kt
+++ sample/src/main/java/org/chx/kdroid/sample/MainActivity.kt
@@ -6,7 +6,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item.view.*
 import org.chx.kandroid.R
 import org.chx.kdroid.kadapter.KAdapter
-import org.chx.kdroid.kadapter.adapter.listAdapter
+import org.chx.kdroid.kadapter.adapter.adapt
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +14,9 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         val dataList = (1..100).map { Pair("Title$it", "Description$it") }
-        vListView.adapter = KAdapter.singleLayout(dataList, R.layout.item) { data, _ ->
+        KAdapter.singleLayout(dataList, R.layout.item) { data, _ ->
             vTitle.text = data.first
             vDescription.text = data.second
-        }.listAdapter()
+        }.adapt(vListView)
     }
 }
