3a3eb68c548cfee5ded99f757c432f2b688f79f5
7hens <thenstango@live.com>
2020-05-20 23:55:40 +0800

    refactor: remove MergeAdapter

--- kdroid/src/main/java/cn/thens/kdroid/vadapter/VAdapter.kt
+++ kdroid/src/main/java/cn/thens/kdroid/vadapter/VAdapter.kt
@@ -37,6 +37,10 @@ interface VAdapter {
         return VRecyclerAdapter(this)
     }
 
+    fun mergeWith(adapter: VAdapter): VAdapter {
+        return merge(this, adapter)
+    }
+
     fun ViewGroup.inflate(layoutRes: Int): View {
         return LayoutInflater.from(context).inflate(layoutRes, this, false)
     }
@@ -62,6 +66,28 @@ interface VAdapter {
 
                 override fun createHolder(container: ViewGroup, itemType: Int): Holder {
                     return creators[itemType](container)
+                }
+            }
+        }
+
+        fun merge(vararg adapters: VAdapter): VAdapter {
+            return object : VAdapter {
+                override val itemCount: Int
+                    get() = adapters.fold(0) { acc, v -> acc + v.itemCount }
+
+                override fun getItemType(position: Int): Int {
+                    return position
+                }
+
+                override fun createHolder(container: ViewGroup, itemType: Int): Holder {
+                    var position = itemType
+                    adapters.forEach { adapter ->
+                        if (position < adapter.itemCount) {
+                            return adapter.createHolder(container, adapter.getItemType(position))
+                        }
+                        position -= adapter.itemCount
+                    }
+                    throw IndexOutOfBoundsException("expect $itemType, actual $itemCount")
                 }
             }
         }
