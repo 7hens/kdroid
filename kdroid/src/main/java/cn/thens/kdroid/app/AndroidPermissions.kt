a40f2a89cae6c0402f22b3d3ca928bf15235805a
7hens <thenstango@live.com>
2020-05-22 22:36:08 +0800

    feat: AndroidPermissions

---
+++ kdroid/src/main/java/cn/thens/kdroid/app/AndroidPermissions.kt
@@ -0,0 +1,46 @@
+package cn.thens.kdroid.app
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object AndroidPermissions {
+    suspend fun requestAll(context: Context, vararg permissions: String): Map<String, Boolean> {
+        val surePermissions = mutableMapOf<String, Boolean>()
+        val notSurePermissions = mutableListOf<String>()
+        permissions.forEach { permission ->
+            if (isGranted(context, permission)) {
+                surePermissions[permission] = true
+            } else if (context is Activity && !shouldRequest(context, permission)) {
+                surePermissions[permission] = false
+            } else {
+                notSurePermissions.add(permission)
+            }
+        }
+        ActivityRequest(context).permissions(*notSurePermissions.toTypedArray())
+                .toMap(surePermissions)
+        return surePermissions
+    }
+
+    suspend fun request(context: Context, permission: String): Boolean {
+        if (isGranted(context, permission)) return true
+        if (context is Activity && !shouldRequest(context, permission)) {
+            return false
+        }
+        return ActivityRequest(context).permission(permission)
+    }
+
+    fun shouldRequest(activity: Activity, permission: String): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)
+    }
+
+    fun isGranted(context: Context, vararg permissions: String): Boolean {
+        return permissions.all { isGranted(ContextCompat.checkSelfPermission(context, it)) }
+    }
+
+    fun isGranted(value: Int): Boolean {
+        return value == PackageManager.PERMISSION_GRANTED
+    }
+}
